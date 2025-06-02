4f21193f8fb5d8b4f076c7cd978e758e218f3e38
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-06-02 15:49:43 -0500

    first commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Isaac Urbina on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
