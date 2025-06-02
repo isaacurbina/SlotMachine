6a22679eae721ee2cb4cb7c700190d66161db348
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-06-02 16:25:42 -0500

    175. 2. Let's build up the Slot Machine minigame's header view (#2)

--- SlotMachine/ContentView.swift
+++ SlotMachine/ContentView.swift
@@ -8,17 +8,120 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	
+	// MARK: - properties
+	
+	
+	
+	
+	// MARK: - body
+	
+	var body: some View {
+		ZStack {
+			
+			
+			// MARK: - background
+			
+			LinearGradient(
+				gradient: Gradient(
+					colors: [Color("ColorPink"), Color("ColorPurple")]),
+				startPoint: .top,
+				endPoint: .bottom
+			)
+			.edgesIgnoringSafeArea(.all)
+			
+			
+			// MARK: - interface
+			
+			VStack(alignment: .center, spacing: 5) {
+				
+				
+				// MARK: - header
+				
+				LogoView()
+				Spacer()
+				
+				
+				// MARK: - score
+				
+				HStack {
+					HStack {
+						Text("Your\nCoins".uppercased())
+							.scoreLabelStyle()
+							.multilineTextAlignment(.trailing)
+						
+						Text("100")
+							.scoreNumberStyle()
+							.modifier(ScoreNumberModifier())
+						
+					} // HStack
+					.modifier(ScoreContainerModifier())
+					
+					Spacer()
+					
+					HStack {
+						Text("200")
+							.scoreNumberStyle()
+							.modifier(ScoreNumberModifier())
+						
+						Text("High\nScore".uppercased())
+							.scoreLabelStyle()
+							.multilineTextAlignment(.leading)
+						
+					} // HStack
+					.modifier(ScoreContainerModifier())
+				}
+				
+				Spacer()
+				
+				
+				// MARK: - slot machine
+				
+				
+				
+				// MARK: - footer
+				
+				
+			} // VStack
+			
+			
+			// MARK: - buttons
+			
+			.overlay(
+				// reset
+				Button(action: {
+					print("Reset the game")
+				}) {
+					Image(systemName: "arrow.2.circlepath.circle")
+				}
+					.modifier(ButtonModifier()),
+				alignment: .topLeading
+			)
+			.overlay(
+				// info
+				Button(action: {
+					print("Info View")
+				}) {
+					Image(systemName: "info.circle")
+				}
+					.modifier(ButtonModifier()),
+				alignment: .topTrailing
+			)
+			.padding()
+			.frame(maxWidth: 720)
+			
+			
+			// MARK: - popup
+			
+			
+		} // ZStack
+	}
 }
 
+
+// MARK: - preview
+
 #Preview {
-    ContentView()
+	ContentView()
 }
---
+++ SlotMachine/Helpers/Modifiers.swift
@@ -0,0 +1,54 @@
+//
+//  Modifiers.swift
+//  SlotMachine
+//
+//  Created by Isaac Urbina on 6/2/25.
+//
+
+import SwiftUI
+
+struct ShadowModifier: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.shadow(
+				color: Color("ColorTransparentBlack"),
+				radius: 0,
+				x: 0,
+				y: 6
+			)
+	}
+}
+
+struct ButtonModifier: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.font(.title)
+			.accentColor(.white)
+	}
+}
+
+struct ScoreNumberModifier: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.shadow(
+				color: Color("ColorTransparentBlack"),
+				radius: 0,
+				x: 0,
+				y: 3
+			)
+			.layoutPriority(1)
+	}
+}
+
+struct ScoreContainerModifier: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.padding(.vertical, 4)
+			.padding(.horizontal, 16)
+			.frame(minWidth: 128)
+			.background(
+				Capsule()
+					.foregroundColor(Color("ColorTransparentBlack"))
+			)
+	}
+}
