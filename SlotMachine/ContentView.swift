31a77abe9dee4a0f1e7eff74406c219d56ef1360
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-06-10 14:57:12 -0500

    183. 10. Add animation to the Slot Machine game using SwiftUI

--- SlotMachine/ContentView.swift
+++ SlotMachine/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
 	@State private var isActiveBet10: Bool = true
 	@State private var isActiveBet20: Bool = false
 	@State private var showingModal: Bool = false
+	@State private var animatingSymbol: Bool = false
+	@State private var animatingModal: Bool = false
 	
 	private let symbols = ["gfx-bell", "gfx-cherry", "gfx-coin", "gfx-grape", "gfx-seven", "gfx-strawberry"]
 	
@@ -160,6 +162,12 @@ struct ContentView: View {
 						Image(symbols[reels[0]])
 							.resizable()
 							.modifier(ImageModifier())
+							.opacity(animatingSymbol ? 1 : 0)
+							.offset(y: animatingSymbol ? 0 : -50)
+							.animation(.easeOut(duration: Double.random(in: 0.5...0.7)))
+							.onAppear {
+								animatingSymbol.toggle()
+							}
 					} // ZStack
 					
 					HStack(alignment: .center, spacing: 0) {
@@ -172,6 +180,12 @@ struct ContentView: View {
 							Image(symbols[reels[1]])
 								.resizable()
 								.modifier(ImageModifier())
+								.opacity(animatingSymbol ? 1 : 0)
+								.offset(y: animatingSymbol ? 0 : -50)
+								.animation(.easeOut(duration: Double.random(in: 0.7...0.9)))
+								.onAppear {
+									animatingSymbol.toggle()
+								}
 						} // ZStack
 						
 						Spacer()
@@ -184,6 +198,12 @@ struct ContentView: View {
 							Image(symbols[reels[2]])
 								.resizable()
 								.modifier(ImageModifier())
+								.opacity(animatingSymbol ? 1 : 0)
+								.offset(y: animatingSymbol ? 0 : -50)
+								.animation(.easeOut(duration: Double.random(in: 0.9...1.1)))
+								.onAppear {
+									animatingSymbol.toggle()
+								}
 						} // ZStack
 						
 					} // HStack
@@ -192,7 +212,13 @@ struct ContentView: View {
 					// MARK: - spin button
 					
 					Button(action: {
+						withAnimation {
+							animatingSymbol = false
+						}
 						spinReels()
+						withAnimation {
+							animatingSymbol = true
+						}
 						checkWinning()
 						isGameOver()
 					}) {
@@ -227,16 +253,19 @@ struct ContentView: View {
 						
 						Image("gfx-casino-chips")
 							.resizable()
+							.offset(x: isActiveBet20 ? 0 : 20)
 							.opacity(isActiveBet20 ? 1 : 0)
 							.modifier(CasinoChipsModifier())
 					} // HStack
 					
+					Spacer()
 					
 					// MARK: - bet 10
 					
 					HStack(alignment: .center, spacing: 10) {
 						Image("gfx-casino-chips")
 							.resizable()
+							.offset(x: isActiveBet10 ? 0 : -20)
 							.opacity(isActiveBet10 ? 1 : 0)
 							.modifier(CasinoChipsModifier())
 						
@@ -326,6 +355,8 @@ struct ContentView: View {
 							
 							Button(action: {
 								showingModal = false
+								animatingModal = false
+								activateBet10()
 								coins = 100
 							}) {
 								Text("New Game".uppercased())
@@ -350,6 +381,12 @@ struct ContentView: View {
 					.background(.white)
 					.cornerRadius(20)
 					.shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+					.opacity($animatingModal.wrappedValue ? 1 : 0)
+					.offset(y: $animatingModal.wrappedValue ? 0 : -100)
+					.animation(.spring(response: 0.6, dampingFraction: 1.0, blendDuration: 1.0))
+					.onAppear {
+						animatingModal = true
+					}
 					
 				} // ZStack
 			} // if
